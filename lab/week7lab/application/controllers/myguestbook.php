b7a46cf28f79972935e0709ec27f6ac684105ee4
ViceNish <76869463+ViceNish@users.noreply.github.com>
2021-05-18 13:40:00 +0800

    hi

--- lab/week7lab/application/controllers/myguestbook.php
+++ lab/week7lab/application/controllers/myguestbook.php
@@ -56,6 +56,53 @@ class MyGuestBook extends CI_Controller {
             $this->load->view('form');  
         }
     }
+
+    public function edit() {
+  
+        if ($this->input->post('form-submitted') == "edit") {
+            $id = $this->uri->segment(3);
+            $name = $this->input->post("name");
+            $email = $this->input->post("email");
+            $comment = $this->input->post("comment");
+ 
+            $this->form_validation->set_rules("name", "Name", "trim|required");
+            $this->form_validation->set_rules("email", "Email", "trim|valid_email|required");
+            $this->form_validation->set_rules("comment", "Comment", "trim|required");
+  
+            if ($this->form_validation->run() == FALSE) {
+                $data['result'] = $this->myguestbook_model->read(array('id' => $id), null, null);
+                $this->load->view('edit', $data);
+            }
+            else {
+                $postdate = date("Y-m-d",time());
+                $posttime = date("H:i:s", time());
+      
+                $data = array(
+                    'user'   => $name,
+                    'email'  => $email,
+                    'comment' => $comment,
+                    'postdate' => $postdate,
+                    'posttime' => $posttime
+                );
+      
+                $this->myguestbook_model->update($id, $data);
+                $this->view();
+            }
+        }
+        else {
+            $id = $this->uri->segment(3);
+            $data['result'] = $this->myguestbook_model->read(array('id' => $id), null, null);
+            $this->load->view('edit', $data);   
+        }
+    }
+
+    public function remove() {
+  
+        $id = $this->uri->segment(3);
+        $this->myguestbook_model->delete($id);
+        $this->view();
+    }
+
 }
  
 ?>
