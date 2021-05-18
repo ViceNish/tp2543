f6ab8ad0ffc42135fd1f990ff19acd3e06ead4fa
ViceNish <76869463+ViceNish@users.noreply.github.com>
2021-05-18 09:42:59 +0800

    hi

--- lab/week7lab/application/controllers/myguestbook.php
+++ lab/week7lab/application/controllers/myguestbook.php
@@ -7,11 +7,18 @@ class MyGuestBook extends CI_Controller {
     function __construct() {
       parent::__construct();
       $this->load->helper('url');
+      $this->load->model('myguestbook_model');
     }
  
     function index() {
       $data['title'] = 'MyGuestBook';
       $this->load->view('mainmenu', $data);
+    }
+
+    public function view() {
+      $data['result'] = $this->myguestbook_model->read(null, null, null);
+      $data['title'] = 'List of All Comments';
+      $this->load->view('list', $data);
     }
 }
  
