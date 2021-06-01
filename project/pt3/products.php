25a1954c43ca5315e9bd45780c3a77ad9a808477
ViceNish <76869463+ViceNish@users.noreply.github.com>
2021-06-01 19:00:01 +0800

    hi

--- project/pt3/products.php
+++ project/pt3/products.php
@@ -5,64 +5,142 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <title>My Bike Ordering System : Products</title>
+  <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-  <center>
-    <a href="index.php">Home</a> |
+  <!-- <center> -->
+    <!-- <a href="index.php">Home</a> |
     <a href="products.php">Products</a> |
     <a href="customers.php">Customers</a> |
     <a href="staffs.php">Staffs</a> |
     <a href="orders.php">Orders</a>
-    <hr>
-    <form action="products.php" method="post">
-      Product ID
-      <input name="pid" type="text" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_product_num']; ?>"> <br>
-      Name
-      <input name="name" type="text" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_product_name']; ?>"> <br>
-      Price
-      <input name="price" type="text" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_product_price']; ?>"> <br>
-      Brand
-      <select name="brand">
+    <hr> -->
+    <?php include_once 'nav_bar.php'; ?>
+
+    <div class="container-fluid">
+
+    <div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <div class="page-header">
+        <h2>Create New Product</h2>
+      </div>
+        
+    <form action="products.php" method="post" class="form-horizontal">
+      <div class="form-group">
+          <label for="productid" class="col-sm-3 control-label">ID</label>
+          <div class="col-sm-9">
+      <input name="pid" type="text" class="form-control" id="productid" placeholder="Product ID" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_product_num']; ?>" required>
+      </div>
+        </div>
+      <div class="form-group">
+          <label for="productname" class="col-sm-3 control-label">Name</label>
+          <div class="col-sm-9">
+      <input name="name" type="text" class="form-control" id="productname" placeholder="Product Name" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_product_name']; ?>" required>
+      </div>
+        </div>
+        <div class="form-group">
+          <label for="productprice" class="col-sm-3 control-label">Price</label>
+          <div class="col-sm-9">
+      <input name="price" type="number" class="form-control" id="productprice" placeholder="Product Price" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_product_price']; ?>" required>
+      </div>
+        </div>
+      <div class="form-group">
+          <label for="productbrand" class="col-sm-3 control-label">Brand</label>
+          <div class="col-sm-9">
+      <select name="brand" class="form-control" id="productbrand" required>
+        <option value="">Please select</option>
         <option value="Kawasaki" <?php if(isset($_GET['edit'])) if($editrow['fld_product_brand']=="Kawasaki") echo "selected"; ?>>Kawasaki</option>
         <option value="Honda" <?php if(isset($_GET['edit'])) if($editrow['fld_product_brand']=="Honda") echo "selected"; ?>>Honda</option>
         <option value="Suzuki" <?php if(isset($_GET['edit'])) if($editrow['fld_product_brand']=="Suzuki") echo "selected"; ?>>Suzuki</option>
-      </select> <br>
-      Condition
-      <input name="cond" type="radio" value="New" <?php if(isset($_GET['edit'])) if($editrow['fld_product_condition']=="New") echo "checked"; ?>> New
-      <input name="cond" type="radio" value="Used" <?php if(isset($_GET['edit'])) if($editrow['fld_product_condition']=="Used") echo "checked"; ?>> Used <br>
-      Manufacturing Year
-      <select name="year">
+      </select>
+      </div>
+        </div>    
+        <div class="form-group">
+          <label for="productcond" class="col-sm-3 control-label">Condition</label>
+          <div class="col-sm-9">
+          <div class="radio">
+            <label>
+      <input name="cond" type="radio" id="productcond" value="New" <?php if(isset($_GET['edit'])) if($editrow['fld_product_condition']=="New") echo "checked"; ?> required> New
+      </label>
+          </div>
+          <div class="radio">
+            <label>
+      <input name="cond" type="radio" id="productcond" value="Used" <?php if(isset($_GET['edit'])) if($editrow['fld_product_condition']=="Used") echo "checked"; ?>> Used 
+      </label>
+            </div>
+          </div>
+      </div>
+        <div class="form-group">
+          <label for="productyear" class="col-sm-3 control-label">Manufacturing Year</label>
+          <div class="col-sm-9">
+      <select name="year" class="form-control" id="productyear" required>
+        <option value="">Please select</option>
         <option value="2013" <?php if(isset($_GET['edit'])) if($editrow['fld_product_year']=="2013") echo "selected"; ?>>2013</option>
         <option value="2014" <?php if(isset($_GET['edit'])) if($editrow['fld_product_year']=="2014") echo "selected"; ?>>2014</option>
         <option value="2015" <?php if(isset($_GET['edit'])) if($editrow['fld_product_year']=="2015") echo "selected"; ?>>2015</option>
-      </select> <br>
-      Quantity
-      <input name="quantity" type="text" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_product_quantity']; ?>"> <br>
+      </select>
+      </div>
+        </div>  
+      <div class="form-group">
+          <label for="productq" class="col-sm-3 control-label">Quantity</label>
+          <div class="col-sm-9">
+      <input name="quantity" type="number" class="form-control" id="productq" placeholder="Product Quantity" value="<?php if(isset($_GET['edit'])) echo $editrow['fld_product_quantity']; ?>" required>
+      </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-3 col-sm-9">
       <?php if (isset($_GET['edit'])) { ?>
       <input type="hidden" name="oldpid" value="<?php echo $editrow['fld_product_num']; ?>">
-      <button type="submit" name="update">Update</button>
+      <button class="btn btn-default" type="submit" name="update"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Update</button>
       <?php } else { ?>
-      <button type="submit" name="create">Create</button>
+      <button class="btn btn-default" type="submit" name="create"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Create</button>
       <?php } ?>
-      <button type="reset">Clear</button>
+      <button class="btn btn-default" type="reset"><span class="glyphicon glyphicon-erase" aria-hidden="true"></span> Clear</button>
+      </div>
+      </div>
     </form>
+  </div>
+  </div>
 
-    <hr>
-    <table border="1">
+
+<div class="row">
+    <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+      <div class="page-header">
+        <h2>Products List</h2>
+      </div>
+      <table class="table table-striped table-bordered">
       <tr>
-        <td>Product ID</td>
-        <td>Name</td>
-        <td>Price</td>
-        <td>Brand</td>
-        <td></td>
+        <th>Product ID</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Brand</th>
+          <th></th>
       </tr>
       <?php
       // Read
+      $per_page = 5;
+      if (isset($_GET["page"]))
+        $page = $_GET["page"];
+      else
+        $page = 1;
+      $start_from = ($page-1) * $per_page;
       try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          $stmt = $conn->prepare("SELECT * FROM tbl_products_a174088");
+          $stmt = $conn->prepare("SELECT * FROM tbl_products_a174088 LIMIT $start_from, $per_page");
         $stmt->execute();
         $result = $stmt->fetchAll();
       }
@@ -77,9 +155,9 @@
         <td><?php echo $readrow['fld_product_price']; ?></td>
         <td><?php echo $readrow['fld_product_brand']; ?></td>
         <td>
-          <a href="products_details.php?pid=<?php echo $readrow['fld_product_num']; ?>">Details</a>
-          <a href="products.php?edit=<?php echo $readrow['fld_product_num']; ?>">Edit</a>
-          <a href="products.php?delete=<?php echo $readrow['fld_product_num']; ?>" onclick="return confirm('Are you sure to delete?');">Delete</a>
+          <a href="products_details.php?pid=<?php echo $readrow['fld_product_num']; ?>" class="btn btn-warning btn-xs" role="button">Details</a>
+          <a href="products.php?edit=<?php echo $readrow['fld_product_num']; ?>" class="btn btn-success btn-xs" role="button">Edit</a>
+          <a href="products.php?delete=<?php echo $readrow['fld_product_num']; ?>" onclick="return confirm('Are you sure to delete?');" class="btn btn-danger btn-xs" role="button">Delete</a>
         </td>
       </tr>
       <?php
@@ -87,6 +165,53 @@
       $conn = null;
       ?>
     </table>
-  </center>
+    </div>
+
+    <div class="row">
+    <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+      <nav>
+          <ul class="pagination">
+          <?php
+          try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT * FROM tbl_products_a174088");
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            $total_records = count($result);
+          }
+          catch(PDOException $e){
+                echo "Error: " . $e->getMessage();
+          }
+          $total_pages = ceil($total_records / $per_page);
+          ?>
+          <?php if ($page==1) { ?>
+            <li class="disabled"><span aria-hidden="true">«</span></li>
+          <?php } else { ?>
+            <li><a href="products.php?page=<?php echo $page-1 ?>" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+          <?php
+          }
+          for ($i=1; $i<=$total_pages; $i++)
+            if ($i == $page)
+              echo "<li class=\"active\"><a href=\"products.php?page=$i\">$i</a></li>";
+            else
+              echo "<li><a href=\"products.php?page=$i\">$i</a></li>";
+          ?>
+          <?php if ($page==$total_pages) { ?>
+            <li class="disabled"><span aria-hidden="true">»</span></li>
+          <?php } else { ?>
+            <li><a href="products.php?page=<?php echo $page+1 ?>" aria-label="Previous"><span aria-hidden="true">»</span></a></li>
+          <?php } ?>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <!-- </center> -->
+
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- project/pt3/products_details.php
+++ project/pt3/products_details.php
@@ -5,16 +5,30 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <title>My Bike Ordering System : Products Details</title>
+  <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-  <center>
-    <a href="index.php">Home</a> |
+  <!-- <center> -->
+    <?php include_once 'nav_bar.php'; ?>
+    <!-- <a href="index.php">Home</a> |
     <a href="products.php">Products</a> |
     <a href="customers.php">Customers</a> |
     <a href="staffs.php">Staffs</a> |
     <a href="orders.php">Orders</a>
-    <hr>
+    <hr> -->
     <?php
     try {
       $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -30,14 +44,61 @@
     }
     $conn = null;
     ?>
-    Product ID: <?php echo $readrow['fld_product_num'] ?> <br>
-    Name: <?php echo $readrow['fld_product_name'] ?> <br>
-    Price: RM <?php echo $readrow['fld_product_price'] ?> <br>
-    Brand: <?php echo $readrow['fld_product_brand'] ?> <br>
-    Condition: <?php echo $readrow['fld_product_condition'] ?> <br>
-    Manufacturing Year: <?php echo $readrow['fld_product_year'] ?> <br>
-    Quantity: <?php echo $readrow['fld_product_quantity'] ?> <br>
-    <img src="products/<?php echo $readrow['fld_product_image'] ?>" width="50%" height="50%">
-  </center>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-12 col-sm-5 col-sm-offset-1 col-md-4 col-md-offset-2 well well-sm text-center">
+      <?php if ($readrow['fld_product_image'] == "" ) {
+        echo "No image";
+      }
+      else { ?>
+      <img src="products/<?php echo $readrow['fld_product_image'] ?>" class="img-responsive">
+      <?php } ?>
+    </div>
+    <div class="col-xs-12 col-sm-5 col-md-4">
+      <div class="panel panel-default">
+      <div class="panel-heading"><strong>Product Details</strong></div>
+      <div class="panel-body">
+          Below are specifications of the product.
+      </div>
+      <table class="table">
+        <tr>
+          <td class="col-xs-4 col-sm-4 col-md-4"><strong>Product ID</strong></td>
+          <td><?php echo $readrow['fld_product_num'] ?></td>
+        </tr>
+        <tr>
+          <td><strong>Name</strong></td>
+          <td><?php echo $readrow['fld_product_name'] ?></td>
+        </tr>
+        <tr>
+          <td><strong>Price</strong></td>
+          <td>RM <?php echo $readrow['fld_product_price'] ?></td>
+        </tr>
+        <tr>
+          <td><strong>Brand</strong></td>
+          <td><?php echo $readrow['fld_product_brand'] ?></td>
+        </tr>
+        <tr>
+          <td><strong>Condition</strong></td>
+          <td><?php echo $readrow['fld_product_condition'] ?></td>
+        </tr>
+        <tr>
+          <td><strong>Manufacturing Year</strong></td>
+          <td><?php echo $readrow['fld_product_year'] ?></td>
+        </tr>
+        <tr>
+          <td><strong>Quantity</strong></td>
+          <td><?php echo $readrow['fld_product_quantity'] ?></td>
+        </tr>
+      </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  <!-- </center> -->
 </body>
 </html>
