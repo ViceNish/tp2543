6a4369649b5270e0c822cb043dd2e15abfaca8d7
ViceNish <76869463+ViceNish@users.noreply.github.com>
2021-06-28 22:13:50 +0800

    bug fixed with id

--- project/myPT4/products.php
+++ project/myPT4/products.php
@@ -22,12 +22,12 @@ include_once 'products_crud.php';
     <![endif]-->
       <link rel="stylesheet" type="text/css" href="body.css">
       <style>
-        input[type="file"] {
-            display: none;
-        }
+      input[type="file"] {
+        display: none;
+      }
     </style>
-    </head>
-    <body>
+  </head>
+  <body>
   <!-- <center>
     <a href="index.php">Home</a> |
     <a href="products.php">Products</a> |
@@ -56,19 +56,19 @@ include_once 'products_crud.php';
           </div>
           <div class="row">
             <form action="<?php echo($_SERVER['REQUEST_URI']); ?>" method="post" class="form-horizontal" enctype="multipart/form-data">
-            <!-- Image -->
-            <div class="col-md-4">
-              <div class="thumbnail dark-1" style="background-color: #696969;">
-                <img src="products/<?php echo (isset($_GET['edit']) ? $editrow['FLD_IMAGE'] : '') ?>" onerror="this.onerror=null;this.src='products/noimage.png';" id="pphoto" alt="Product Image" style="width: 100%;height: 100%;">
-                <div class="caption text-center">
-                  <h3 id="productImageTitle" style="word-break: break-all;color:#fbc306;"><?php echo (isset($_GET['edit']) && $editrow['FLD_IMAGE'] !== '' ? explode('.', $editrow['FLD_IMAGE'])[0] : 'Product Image') ?></h3> <!-- explode like split, it will remove the element after . -->
-                  <p>
-                    <label class="btn btn-primary">
-                      <input type="file" accept="images/*" name="fileToUpload" id="inputImage" onchange="loadFile(event);" />
-                      <i class="fa fa-cloud-upload"></i>Upload
+              <!-- Image -->
+              <div class="col-md-4">
+                <div class="thumbnail dark-1" style="background-color: #696969;">
+                  <img src="products/<?php echo (isset($_GET['edit']) ? $editrow['FLD_IMAGE'] : '') ?>" onerror="this.onerror=null;this.src='products/noimage.png';" id="pphoto" alt="Product Image" style="width: 100%;height: 100%;">
+                  <div class="caption text-center">
+                    <h3 id="productImageTitle" style="word-break: break-all;color:#fbc306;"><?php echo (isset($_GET['edit']) && $editrow['FLD_IMAGE'] !== '' ? explode('.', $editrow['FLD_IMAGE'])[0] : 'Product Image') ?></h3> <!-- explode like split, it will remove the element after . -->
+                    <p>
+                      <label class="btn btn-primary">
+                        <input type="file" accept="images/*" name="fileToUpload" id="inputImage" onchange="loadFile(event);" />
+                        <i class="fa fa-cloud-upload"></i>Upload
 
-                    </label>
-                    <?php
+                      </label>
+                      <?php
                     /*if (isset($_GET['edit']) && $editrow['FLD_IMAGE'] != '') {
                       echo '<a href="#" class="btn btn-danger disabled" role="button">Delete</a>';
                     }*/
@@ -79,134 +79,134 @@ include_once 'products_crud.php';
             </div>
             <div class="col-md-8">
               
-            <div class="form-group">
-              <label for="pid" class="col-sm-3 control-label">Product ID</label>
-              <div class="col-sm-9">
-                <input name="pid" class="form-control" type="text" id="pid" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_PRODUCT_ID']; else echo sprintf('F%02d',$ppid); ?>" readonly>
+              <div class="form-group">
+                <label for="pid" class="col-sm-3 control-label">Product ID</label>
+                <div class="col-sm-9">
+                  <input name="pid" class="form-control" type="text" id="pid" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_PRODUCT_ID']; else echo sprintf('F%02d',$ppid); ?>" readonly>
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <label for="productname" class="col-sm-3 control-label">Name</label>
-              <div class="col-sm-9">
-                <input name="name" class="form-control" type="text" id="productname" placeholder="Product Name" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_PRODUCT_NAME']; ?>" required> 
+              <div class="form-group">
+                <label for="productname" class="col-sm-3 control-label">Name</label>
+                <div class="col-sm-9">
+                  <input name="name" class="form-control" type="text" id="productname" placeholder="Product Name" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_PRODUCT_NAME']; ?>" required> 
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <label for="productprice" class="col-sm-3 control-label">Price</label>
-              <div class="col-sm-9">
-                <input name="price" type="number" min="0.00" step="0.01" class="form-control" id="productprice" placeholder="Product Price" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_PRICE']; ?>" required> 
+              <div class="form-group">
+                <label for="productprice" class="col-sm-3 control-label">Price</label>
+                <div class="col-sm-9">
+                  <input name="price" type="number" min="0.00" step="0.01" class="form-control" id="productprice" placeholder="Product Price" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_PRICE']; ?>" required> 
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <label for="brand" class="col-sm-3 control-label">Brand</label>
-              <div class="col-sm-9">
-                <input type="text" name="brand" class="form-control" id="brand" placeholder="Brand" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_BRAND']; ?>" required> 
+              <div class="form-group">
+                <label for="brand" class="col-sm-3 control-label">Brand</label>
+                <div class="col-sm-9">
+                  <input type="text" name="brand" class="form-control" id="brand" placeholder="Brand" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_BRAND']; ?>" required> 
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <label for="type" class="col-sm-3 control-label">Type</label>
-              <div class="col-sm-9">
-                <select name="type" class="form-control" id="type" required>
-                  <option value="">Please select</option>
-                  <option value="Table" <?php if(isset($_GET['edit'])) if($editrow['FLD_TYPE']=="Table") echo "selected"; ?>>Table</option>
-                  <option value="Chair" <?php if(isset($_GET['edit'])) if($editrow['FLD_TYPE']=="Chair") echo "selected"; ?>>Chair</option>
-                  <option value="Drawer" <?php if(isset($_GET['edit'])) if($editrow['FLD_TYPE']=="Drawer") echo "selected"; ?>>Drawer</option>
-                  <option value="Shelf" <?php if(isset($_GET['edit'])) if($editrow['FLD_TYPE']=="Shelf") echo "selected"; ?>>Shelf</option>
-                </select> 
+              <div class="form-group">
+                <label for="type" class="col-sm-3 control-label">Type</label>
+                <div class="col-sm-9">
+                  <select name="type" class="form-control" id="type" required>
+                    <option value="">Please select</option>
+                    <option value="Table" <?php if(isset($_GET['edit'])) if($editrow['FLD_TYPE']=="Table") echo "selected"; ?>>Table</option>
+                    <option value="Chair" <?php if(isset($_GET['edit'])) if($editrow['FLD_TYPE']=="Chair") echo "selected"; ?>>Chair</option>
+                    <option value="Drawer" <?php if(isset($_GET['edit'])) if($editrow['FLD_TYPE']=="Drawer") echo "selected"; ?>>Drawer</option>
+                    <option value="Shelf" <?php if(isset($_GET['edit'])) if($editrow['FLD_TYPE']=="Shelf") echo "selected"; ?>>Shelf</option>
+                  </select> 
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <label for="qty" class="col-sm-3 control-label">Quantity</label>
-              <div class="col-sm-9">
-                <input name="quantity" type="number" min="1" step="1" class="form-control" id="qty" placeholder="Quantity" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_QUANTITY']; ?>" required> 
+              <div class="form-group">
+                <label for="qty" class="col-sm-3 control-label">Quantity</label>
+                <div class="col-sm-9">
+                  <input name="quantity" type="number" min="1" step="1" class="form-control" id="qty" placeholder="Quantity" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_QUANTITY']; ?>" required> 
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <label for="colour" class="col-sm-3 control-label">Colour</label>
-              <div class="col-sm-9">
-                <input name="colour" type="text" class="form-control" id="colour" placeholder="Colour" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_COLOUR']; ?>" required> 
+              <div class="form-group">
+                <label for="colour" class="col-sm-3 control-label">Colour</label>
+                <div class="col-sm-9">
+                  <input name="colour" type="text" class="form-control" id="colour" placeholder="Colour" value="<?php if(isset($_GET['edit'])) echo $editrow['FLD_COLOUR']; ?>" required> 
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <div class="col-sm-offset-3 col-sm-9">
-                <!-- <input type="hidden" name="image" value="noimage.png"> --> 
-                <?php if (isset($_GET['edit'])) { ?>
-                  <input type="hidden" name="oldpid" value="<?php echo $editrow['FLD_PRODUCT_ID']; ?>">
-                  <button class="btn btn-default" type="submit" name="update"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Update</button>
-                <?php } else { ?>
-                  <button class="btn btn-default" type="submit" name="create"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Create</button>
-                <?php } ?>
-                <button class="btn btn-default" type="reset"><span class="glyphicon glyphicon-erase" aria-hidden="true"></span> Clear</button>
+              <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-9">
+                  <!-- <input type="hidden" name="image" value="noimage.png"> --> 
+                  <?php if (isset($_GET['edit'])) { ?>
+                    <input type="hidden" name="oldpid" value="<?php echo $editrow['FLD_PRODUCT_ID']; ?>">
+                    <button class="btn btn-default" type="submit" name="update"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Update</button>
+                  <?php } else { ?>
+                    <button class="btn btn-default" type="submit" name="create"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Create</button>
+                  <?php } ?>
+                  <button class="btn btn-default" type="reset"><span class="glyphicon glyphicon-erase" aria-hidden="true"></span> Clear</button>
+                </div>
               </div>
-            </div>
-          </form>
-            </div>
+            </form>
           </div>
         </div>
       </div>
+    </div>
 
 
-      <div class="row">
-        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-          <div class="page-header">
-            <h2>Product List</h2>
-          </div>
-          <table class="table table-bordered">
+    <div class="row">
+      <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+        <div class="page-header">
+          <h2>Product List</h2>
+        </div>
+        <table class="table table-bordered">
+          <tr>
+            <th>Product ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Brand</th>
+            <th>Type</th>
+            <th>Quantity</th>
+            <th>Colour</th>
+            <th></th>
+          </tr>
+          <?php
+      // Read
+          $per_page = 5;
+          if (isset($_GET["page"]))
+            $page = $_GET["page"];
+          else
+            $page = 1;
+          $start_from = ($page-1) * $per_page;
+          try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT * FROM tbl_products_a174088_pt2 ORDER BY FLD_PRODUCT_ID ASC LIMIT $start_from,$per_page");
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+          }
+          catch(PDOException $e){
+            echo "Error: " . $e->getMessage();
+          }
+          foreach($result as $readrow) {
+            ?> 
             <tr>
-              <th>Product ID</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Brand</th>
-              <th>Type</th>
-              <th>Quantity</th>
-              <th>Colour</th>
-              <th></th>
+              <td><?php echo $readrow['FLD_PRODUCT_ID']; ?></td>
+              <td><?php echo $readrow['FLD_PRODUCT_NAME']; ?></td>
+              <td><?php echo 'RM '.$readrow['FLD_PRICE']; ?></td>
+              <td><?php echo $readrow['FLD_BRAND']; ?></td>
+              <td><?php echo $readrow['FLD_TYPE']; ?></td>
+              <td><?php echo $readrow['FLD_QUANTITY']; ?></td>
+              <td><?php echo $readrow['FLD_COLOUR']; ?></td>
+              <td>
+                <a href="products_details.php?pid=<?php echo $readrow['FLD_PRODUCT_ID']; ?>" class="btn btn-warning btn-xs" role="button">Details</a>
+                <a href="products.php?edit=<?php echo $readrow['FLD_PRODUCT_ID']; echo (isset($_GET['page']) ? '&page='.$_GET['page'] : ''); ?>" class="btn btn-success btn-xs" role="button"> Edit </a>
+                <!-- <a href="products.php?edit=<?php echo $readrow['FLD_PRODUCT_ID']; ?>" class="btn btn-success btn-xs" role="button">Edit</a> -->
+                <a href="products.php?delete=<?php echo $readrow['FLD_PRODUCT_ID']; ?>" onclick="return confirm('Are you sure to delete?');" class="btn btn-danger btn-xs" role="button">Delete</a>
+              </td>
             </tr>
             <?php
-      // Read
-            $per_page = 5;
-            if (isset($_GET["page"]))
-              $page = $_GET["page"];
-            else
-              $page = 1;
-            $start_from = ($page-1) * $per_page;
-            try {
-              $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-              $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              $stmt = $conn->prepare("SELECT * FROM tbl_products_a174088_pt2 ORDER BY FLD_PRODUCT_ID ASC LIMIT $start_from,$per_page");
-              $stmt->execute();
-              $result = $stmt->fetchAll();
-            }
-            catch(PDOException $e){
-              echo "Error: " . $e->getMessage();
-            }
-            foreach($result as $readrow) {
-              ?> 
-              <tr>
-                <td><?php echo $readrow['FLD_PRODUCT_ID']; ?></td>
-                <td><?php echo $readrow['FLD_PRODUCT_NAME']; ?></td>
-                <td><?php echo 'RM '.$readrow['FLD_PRICE']; ?></td>
-                <td><?php echo $readrow['FLD_BRAND']; ?></td>
-                <td><?php echo $readrow['FLD_TYPE']; ?></td>
-                <td><?php echo $readrow['FLD_QUANTITY']; ?></td>
-                <td><?php echo $readrow['FLD_COLOUR']; ?></td>
-                <td>
-                  <a href="products_details.php?pid=<?php echo $readrow['FLD_PRODUCT_ID']; ?>" class="btn btn-warning btn-xs" role="button">Details</a>
-                  <a href="products.php?edit=<?php echo $readrow['FLD_PRODUCT_ID']; echo (isset($_GET['page']) ? '&page='.$_GET['page'] : ''); ?>" class="btn btn-success btn-xs" role="button"> Edit </a>
-                  <!-- <a href="products.php?edit=<?php echo $readrow['FLD_PRODUCT_ID']; ?>" class="btn btn-success btn-xs" role="button">Edit</a> -->
-                  <a href="products.php?delete=<?php echo $readrow['FLD_PRODUCT_ID']; ?>" onclick="return confirm('Are you sure to delete?');" class="btn btn-danger btn-xs" role="button">Delete</a>
-                </td>
-              </tr>
-              <?php
-            }
+          }
       // if (!isset($_GET['edit']) && $stmt->rowCount()>0){
       //   $no = ltrim($readrow['FLD_PRODUCT_ID'], 'F')+1;
       //   $no = 'F'.str_pad($no,2,"0",STR_PAD_LEFT);
       // }elseif(!isset($_GET['edit'])){
       //       $no = 'F'.str_pad(1,2,"0",STR_PAD_LEFT);
       // }
-            $conn = null;
-            ?>
+          $conn = null;
+          ?>
     <!-- <script type="text/javascript">
         if("" !== null && "" !== ""){
           var pid = document.getElementById("pid");
--- project/myPT4/products_crud.php
+++ project/myPT4/products_crud.php
@@ -1,11 +1,17 @@
 <?php
- 
+
 include_once 'database.php';
- 
-function uploadPhoto($file){
+
+function uploadPhoto($file,$id){
   $target_dir="products/";
-  $target_file=$target_dir . basename($file["name"]);
-  $imageFileType=strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+  $target_file2=$target_dir . basename($file["name"]);
+  //$namefile= basename($file["name"]);// . explode(".", basename($file["name"][0]));
+  // $nanamefile = explode(".", $namefile["name"][0]);
+  //$file_direct=$target_dir . $namefile;
+  $imageFileType=strtolower(pathinfo($target_file2, PATHINFO_EXTENSION));
+  // $newfilename = "{$namefile}-".microtime().".{$imageFileType}";
+  $newfilename = "{$id}.{$imageFileType}";
+   $target_file = $target_dir . $newfilename;
   /*
    * 0 = image file is a fake image
    * 1 = file is too large.
@@ -37,7 +43,10 @@ function uploadPhoto($file){
     return 3;
   }
 
-  return array('status' => 200, 'name' => basename($file["name"]));
+   
+
+  // return array('status' => 200, 'name' => basename($file["name"]));
+  return array('status' => 200, 'name' => $newfilename,'ext' => $imageFileType);
 
 }
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -47,26 +56,26 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if (isset($_POST['create'])) {
 
- $flag = uploadPhoto($_FILES['fileToUpload']);
+ $flag = uploadPhoto($_FILES['fileToUpload'],$_POST['pid']);
 
  if (isset($flag["status"])) {
 
    try {
- 
-      $stmt = $conn->prepare("INSERT INTO tbl_products_a174088_pt2(FLD_PRODUCT_ID,
-        FLD_PRODUCT_NAME, FLD_PRICE, FLD_BRAND, FLD_TYPE,
-        FLD_QUANTITY, FLD_COLOUR, FLD_IMAGE) VALUES(:pid, :name, :price, :brand,
-        :type, :quantity, :colour, :image)");
-     
-      $stmt->bindParam(':pid', $pid, PDO::PARAM_STR);
-      $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-      $stmt->bindParam(':price', $price, PDO::PARAM_INT);
-      $stmt->bindParam(':brand', $brand, PDO::PARAM_STR);
-      $stmt->bindParam(':type', $type, PDO::PARAM_STR);
-      $stmt->bindParam(':quantity', $quantity, PDO::PARAM_INT);
-      $stmt->bindParam(':colour', $colour, PDO::PARAM_INT);
-      $stmt->bindParam(':image', $flag['name'], PDO::PARAM_STR);
-       
+
+    $stmt = $conn->prepare("INSERT INTO tbl_products_a174088_pt2(FLD_PRODUCT_ID,
+      FLD_PRODUCT_NAME, FLD_PRICE, FLD_BRAND, FLD_TYPE,
+      FLD_QUANTITY, FLD_COLOUR, FLD_IMAGE) VALUES(:pid, :name, :price, :brand,
+      :type, :quantity, :colour, :image)");
+
+    $stmt->bindParam(':pid', $pid, PDO::PARAM_STR);
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':price', $price, PDO::PARAM_INT);
+    $stmt->bindParam(':brand', $brand, PDO::PARAM_STR);
+    $stmt->bindParam(':type', $type, PDO::PARAM_STR);
+    $stmt->bindParam(':quantity', $quantity, PDO::PARAM_INT);
+    $stmt->bindParam(':colour', $colour, PDO::PARAM_INT);
+    $stmt->bindParam(':image', $flag['name'], PDO::PARAM_STR);
+
     $pid = $_POST['pid'];
     $name = $_POST['name'];
     $price = $_POST['price'];
@@ -75,17 +84,22 @@ if (isset($_POST['create'])) {
     $quantity = $_POST['quantity'];
     $colour = $_POST['colour'];
     // $image = $_POST['image'];
-     
+
     $stmt->execute();
 
+// // Rename file after upload
+ //  $id = $conn->query("SELECT MAX(FLD_PRODUCT_ID) AS LASTID FROM tbl_products_a174088_pt2")->fetch()['LASTID'];
+ // rename("products/{$flag['name']}", "products/{$id}.{$flag['ext']}");
+   
+
     $_SESSION['success'] = "Your product have successfully registered.";
-    }
- 
+  }
+
   catch(PDOException $e)
   {
-      echo "Error: " . $e->getMessage();
+     $_SESSION['error'] = $e->getMessage();
   }
- }else{
+}else{
   if ($flag==0) {
     $_SESSION['error'] = "Please make sure the file uploaded is an image.";
     // echo '<script>alert("Error : Please make sure the file uploaded is an image."); </script>';
@@ -100,23 +114,23 @@ if (isset($_POST['create'])) {
   }else{
     $_SESSION['error'] = "An unknown error has been occurred.";
   }
- }
- 
- //header("LOCATION: {$_SERVER['REQUEST_URI']}");
-  // header('LOCATION: products.php');
-  // exit();
 }
- 
+
+ header("LOCATION: {$_SERVER['REQUEST_URI']}");
+  // header('LOCATION: products.php');
+   exit();
+}
+
 //Update
 if (isset($_POST['update'])) {
- 
+
   try {
- 
+
     //   $stmt = $conn->prepare("UPDATE tbl_products_a174088_pt2 SET 
     //     FLD_PRODUCT_NAME = :name, FLD_PRICE = :price, FLD_BRAND = :brand,
     //     FLD_TYPE = :type, FLD_QUANTITY = :quantity, FLD_COLOUR = :colour
     //     WHERE FLD_PRODUCT_ID = :oldpid LIMIT 1");
-     
+
     //   // $stmt->bindParam(':pid', $pid, PDO::PARAM_STR);
     //   $stmt->bindParam(':name', $name, PDO::PARAM_STR);
     //   $stmt->bindParam(':price', $price, PDO::PARAM_INT);
@@ -126,7 +140,7 @@ if (isset($_POST['update'])) {
     //   $stmt->bindParam(':colour', $colour, PDO::PARAM_INT);
     //  // $stmt->bindParam(':image', $flag['name'], PDO::PARAM_STR);
     //   $stmt->bindParam(':oldpid', $oldpid, PDO::PARAM_STR);
-       
+
     // // $pid = $_POST['pid'];
     // $name = $_POST['name'];
     // $price = $_POST['price'];
@@ -136,7 +150,7 @@ if (isset($_POST['update'])) {
     // $colour = $_POST['colour'];
     // // $image = $_POST['image'];
     // $oldpid = $_POST['oldpid'];
-     
+
     // $stmt->execute();
     // $_SESSION['success'] = "Your product have successfully edited.";
 
@@ -148,8 +162,8 @@ if (isset($_POST['update'])) {
       //   WHERE FLD_PRODUCT_ID = :oldpid LIMIT 1");
 
       $sql = "UPDATE tbl_products_a174088_pt2 SET 
-         FLD_PRODUCT_NAME = :name, FLD_PRICE = :price, FLD_BRAND = :brand,
-         FLD_TYPE = :type, FLD_QUANTITY = :quantity, FLD_COLOUR = :colour";
+      FLD_PRODUCT_NAME = :name, FLD_PRICE = :price, FLD_BRAND = :brand,
+      FLD_TYPE = :type, FLD_QUANTITY = :quantity, FLD_COLOUR = :colour";
 
       if (isset($flag['status'])) {
         $sql .= ", FLD_IMAGE = :image";
@@ -164,7 +178,7 @@ if (isset($_POST['update'])) {
       $stmt->bindParam(':type', $type, PDO::PARAM_STR);
       $stmt->bindParam(':quantity', $quantity, PDO::PARAM_INT);
       $stmt->bindParam(':colour', $colour, PDO::PARAM_INT);
-     
+
       $stmt->bindParam(':oldpid', $oldpid, PDO::PARAM_STR);
       $name = $_POST['name'];
       $price = $_POST['price'];
@@ -172,12 +186,12 @@ if (isset($_POST['update'])) {
       $type = $_POST['type'];
       $quantity = $_POST['quantity'];
       $colour = $_POST['colour'];
-    
+
       $oldpid = $_POST['oldpid'];
 
-if (isset($flag['status'])) {
+      if (isset($flag['status'])) {
         $stmt->bindParam(':image', $flag['name']);
-}
+      }
 
       $stmt->execute();
       $_SESSION['success'] = "Your product have successfully edited.";
@@ -192,39 +206,39 @@ if (isset($flag['status'])) {
     }else{
       // unset($_SESSION['success']);
       if ($flag == 0)
-          $_SESSION['error'] = "Please make sure the file uploaded is an image.";
+        $_SESSION['error'] = "Please make sure the file uploaded is an image.";
       elseif ($flag == 1)
-          $_SESSION['error'] = "Sorry, only file with below 10MB are allowed.";
+        $_SESSION['error'] = "Sorry, only file with below 10MB are allowed.";
       elseif ($flag == 2)
-          $_SESSION['error'] = "Sorry, only PNG & GIF files are allowed.";
+        $_SESSION['error'] = "Sorry, only PNG & GIF files are allowed.";
       elseif ($flag == 3)
-          $_SESSION['error'] = "Sorry, there was an error uploading your file.";
+        $_SESSION['error'] = "Sorry, there was an error uploading your file.";
       else
-          $_SESSION['error'] = "An unknown error has been occurred.";
+        $_SESSION['error'] = "An unknown error has been occurred.";
     }
- 
-     
-    }
- 
-  catch(PDOException $e)
-  {
-       $_SESSION['error']=   $e->getMessage();
+
+
   }
 
-  if (isset($_SESSION['error']))
-        header("LOCATION: {$_SERVER['REQUEST_URI']}");
-    else
-        header("Location: products.php");
+  catch(PDOException $e)
+  {
+   $_SESSION['error']=   $e->getMessage();
+ }
+
+ if (isset($_SESSION['error']))
+  header("LOCATION: {$_SERVER['REQUEST_URI']}");
+else
+  header("Location: products.php");
 
   //exit();
   // $_SESSION['success'] = "Your product have successfully edited.";
   // header("Location: products.php");
-  exit();
+exit();
 }
- 
+
 //Delete
 if (isset($_GET['delete'])) {
- 
+
   try {
 
    $pid = $_GET['delete'];
@@ -252,31 +266,31 @@ catch(PDOException $e)
   echo "Error: " . $e->getMessage();
 }
 }
- 
+
 //Edit
 if (isset($_GET['edit'])) {
- 
+
   try {
- 
-      $stmt = $conn->prepare("SELECT * FROM tbl_products_a174088_pt2 WHERE FLD_PRODUCT_ID = :pid");
-     
-      $stmt->bindParam(':pid', $pid, PDO::PARAM_STR);
-       
+
+    $stmt = $conn->prepare("SELECT * FROM tbl_products_a174088_pt2 WHERE FLD_PRODUCT_ID = :pid");
+
+    $stmt->bindParam(':pid', $pid, PDO::PARAM_STR);
+
     $pid = $_GET['edit'];
-     
+
     $stmt->execute();
- 
+
     $editrow = $stmt->fetch(PDO::FETCH_ASSOC);
-    }
- 
+  }
+
   catch(PDOException $e)
   {
-      echo "Error: " . $e->getMessage();
+    echo "Error: " . $e->getMessage();
   }
 }
- 
+
  //next id
- $ppid = $conn->query("SELECT MAX(FLD_PRODUCT_ID) AS LASTID FROM tbl_products_a174088_pt2")->fetch()['LASTID'];
-  $ppid = ltrim($ppid, 'F')+1; 
-  $conn = null;
+$ppid = $conn->query("SELECT MAX(FLD_PRODUCT_ID) AS LASTID FROM tbl_products_a174088_pt2")->fetch()['LASTID'];
+$ppid = ltrim($ppid, 'F')+1; 
+$conn = null;
 ?>
