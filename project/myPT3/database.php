c225259141f3971dcbab2230c51a918174870250
ViceNish <76869463+ViceNish@users.noreply.github.com>
2021-07-06 00:30:02 +0800

    security

--- project/myPT3/database.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
-$servername = "lrgs.ftsm.ukm.my";
-$username = "a174088";
-$password = "littleyellowcamel";
-$dbname = "a174088";
- 
-?>
--- project/myPT4/database.php
+++
@@ -1,9 +0,0 @@
-<?php
- session_start();
-
-$servername = "lrgs.ftsm.ukm.my";
-$username = "a174088";
-$password = "littleyellowcamel";
-$dbname = "a174088";
- 
-?>
